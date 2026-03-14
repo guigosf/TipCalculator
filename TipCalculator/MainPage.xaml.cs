ecb3dbf20de05ab8c8ba0793e6f1ebe2a3a8b552
Etec Prof. Horácio Augusto da Silveira <guigosf>
2026-03-13 22:36:42 -0300

    TipCalculator

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularButton_Clicked(object sender, EventArgs e)
+        {
+            double refeicao = Double.Parse(RefeicaoEntry.Text);
+            double porcentagem = Double.Parse(PorcentagemEntry.Text);
+            int qtdPessoas = Int32.Parse(QtdPessoasEntry.Text);
+
+            double gorjeta = refeicao * (porcentagem / 100);
+            double valorTotal = refeicao + gorjeta;
+            double valorPessoa = valorTotal / qtdPessoas;
+
+            GorjetaEntry.Text = gorjeta.ToString();
+            ValorPorPessoaEntry.Text = valorPessoa.ToString();
+            ValorTotalEntry.Text = valorTotal.ToString();
+        }
+    }
+}

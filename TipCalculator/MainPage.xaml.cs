595237e66a48d71fbce855a12bf24a3002ace3dd
guigosf <guisf2007@gmail.com>
2026-03-20 22:19:23 -0300

    feat: Adicionado o componente slider e a lógica de tratamento de exceções

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace TipCalculator
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace TipCalculator
 {
     public partial class MainPage : ContentPage
     {
@@ -9,17 +13,32 @@
 
         private void CalcularButton_Clicked(object sender, EventArgs e)
         {
-            double refeicao = Double.Parse(RefeicaoEntry.Text);
-            double porcentagem = Double.Parse(PorcentagemEntry.Text);
-            int qtdPessoas = Int32.Parse(QtdPessoasEntry.Text);
+            //outra forma de realizar conversão
+            //double refeicao = Convert.ToDouble(RefeicaoEntry.Text);
+            try
+            {
+                double refeicao = Double.Parse(RefeicaoEntry.Text);
+                double porcentagem = Double.Parse(PorcentagemEntry.Text);
+                int qtdPessoas = Int32.Parse(QtdPessoasEntry.Text);
 
-            double gorjeta = refeicao * (porcentagem / 100);
-            double valorTotal = refeicao + gorjeta;
-            double valorPessoa = valorTotal / qtdPessoas;
+                double gorjeta = refeicao * (porcentagem / 100);
+                double valorTotal = refeicao + gorjeta;
+                double valorPessoa = valorTotal / qtdPessoas;
 
-            GorjetaEntry.Text = gorjeta.ToString();
-            ValorPorPessoaEntry.Text = valorPessoa.ToString();
-            ValorTotalEntry.Text = valorTotal.ToString();
+                GorjetaEntry.Text = gorjeta.ToString("C");
+                ValorPorPessoaEntry.Text = $"R${valorPessoa}";
+                ValorTotalEntry.Text = valorTotal.ToString("C");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            PorcentagemEntry.Text = value.ToString();
         }
     }
 }
